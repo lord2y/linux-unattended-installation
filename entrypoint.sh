943ee5ad3b3fabc9ad727f5705e7f8196324c939
Alessandro Ratti <alessandro.ratti@exoscale.ch>
2022-12-20 19:38:39 +0100

    Add bash scripts

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,20 @@
 : "${BIN_XORRISO:=$(type -P xorriso)}"
 : "${BIN_7Z:=$(type -P 7z)}"
 
+LIVEFS_EDITOR="${LIVEFS_EDITOR-$(readlink -f "$(dirname $(dirname ${0}))/livefs-editor")}"
+[ -d $LIVEFS_EDITOR ] || git clone https://github.com/mwhudson/livefs-editor $LIVEFS_EDITOR
+
+LIVEFS_EDITOR=$(readlink -f $LIVEFS_EDITOR)
+echo $LIVEFS_EDITOR
+
 _ISO="jammy-live-server-amd64.iso"
 _SOURCE_ISO_URL="https://cdimage.ubuntu.com/ubuntu-server/jammy/daily-live/current/$_ISO"
 _SOURCE="source-files"
 _OUTPUT="/output"
+_PROFILES=(bastion dhcp vpn)
 
 # Create the DIR source-files
-mkdir -p "$_SOURCE/server"
+mkdir -p "$_SOURCE"/{bastion,dhcp,vpn}
 
 if [ ! -f "$_ISO" ];then
 	# Download the ISO
@@ -25,12 +32,15 @@ mv "$_SOURCE"/'[BOOT]' ../BOOT
 
 #Copy files
 cp -v autoinstall/grub/grub.cfg "$_SOURCE"/boot/grub/grub.cfg
-cp -v autoinstall/server/* "$_SOURCE"/server
+for profile in "${_PROFILES[@]}";
+do
+	cp -v autoinstall/"$profile"/* "$_SOURCE/$profile"
 
+done
 cd "$_SOURCE"
 "$BIN_XORRISO" -as mkisofs -r \
   -V 'Ubuntu 22.04 LTS AUTO (EFIBIOS)' \
-  -o ../ubuntu-22.04-autoinstall.iso \
+  -o $_OUTPUT/ubuntu-22.04-autoinstall-tmp.iso \
   --grub2-mbr ../BOOT/1-Boot-NoEmul.img \
   -partition_offset 16 \
   --mbr-force-bootable \
@@ -45,4 +55,9 @@ cd "$_SOURCE"
   -no-emul-boot \
   .
 
-cp -v ../ubuntu-22.04-autoinstall.iso  $_OUTPUT/ubuntu-22.04-autoinstall.iso
+#cp -v ../ubuntu-22.04-autoinstall.iso  $_OUTPUT/ubuntu-22.04-autoinstall.iso
+mount -t tmpfs tmpfs /tmp
+cd "$LIVEFS_EDITOR"
+git apply /patch/livefs.patch
+cd /
+PYTHONPATH=$LIVEFS_EDITOR python3 -m livefs_edit $_OUTPUT/ubuntu-22.04-autoinstall-tmp.iso $_OUTPUT/ubuntu-22.04-autoinstall.iso --add-debs-to-pool $_OUTPUT/puppet-omnibus_3.8.5-exo4_amd64.deb 
