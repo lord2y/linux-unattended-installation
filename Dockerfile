2ca9c1bfbfb5109c285e399077791ac55dd8c1b5
Alessandro Ratti <alessandro.ratti@exoscale.ch>
2022-12-20 23:02:34 +0000

    Stable version

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,27 @@
-FROM ubuntu:22.04
+# vim:set ft=dockerfile:
 
-RUN apt-get update && apt-get install -y xorriso p7zip-full wget
+FROM ubuntu:22.04
+ENV EDITOR vim
+ENV LANG C.UTF-8
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update \ 
+   && apt-get install -y -q \
+   xorriso \
+   p7zip-full \
+   wget \
+   git \ 
+   python3-pip \
+   python3-debian \ 
+   python3-yaml \
+   udev \
+   apt-utils \
+   initramfs-tools-core \
+   squashfs-tools \
+   && apt-get autoremove -y \
+   && apt-get clean \
+   && rm -rf /var/lib/apt/lists/*
 
 COPY grub.cfg autoinstall/grub/grub.cfg
 COPY bastion autoinstall/bastion
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@
 : "${BIN_XORRISO:=$(type -P xorriso)}"
 : "${BIN_7Z:=$(type -P 7z)}"
 
+LIVEFS_EDITOR="${LIVEFS_EDITOR-$(readlink -f "$(dirname $(dirname ${0}))/livefs-editor")}"
+[ -d $LIVEFS_EDITOR ] || git clone https://github.com/mwhudson/livefs-editor $LIVEFS_EDITOR
+
+LIVEFS_EDITOR=$(readlink -f $LIVEFS_EDITOR)
+echo $LIVEFS_EDITOR
+
 _ISO="jammy-live-server-amd64.iso"
 _SOURCE_ISO_URL="https://cdimage.ubuntu.com/ubuntu-server/jammy/daily-live/current/$_ISO"
 _SOURCE="source-files"
@@ -34,7 +40,7 @@ done
 cd "$_SOURCE"
 "$BIN_XORRISO" -as mkisofs -r \
   -V 'Ubuntu 22.04 LTS AUTO (EFIBIOS)' \
-  -o ../ubuntu-22.04-autoinstall.iso \
+  -o $_OUTPUT/ubuntu-22.04-autoinstall-tmp.iso \
   --grub2-mbr ../BOOT/1-Boot-NoEmul.img \
   -partition_offset 16 \
   --mbr-force-bootable \
@@ -49,4 +55,9 @@ cd "$_SOURCE"
   -no-emul-boot \
   .
 
-cp -v ../ubuntu-22.04-autoinstall.iso  $_OUTPUT/ubuntu-22.04-autoinstall.iso
+#cp -v ../ubuntu-22.04-autoinstall.iso  $_OUTPUT/ubuntu-22.04-autoinstall.iso
+mount -t tmpfs tmpfs /tmp
+cd "$LIVEFS_EDITOR"
+git apply /patch/livefs.patch
+cd /
+PYTHONPATH=$LIVEFS_EDITOR python3 -m livefs_edit $_OUTPUT/ubuntu-22.04-autoinstall-tmp.iso $_OUTPUT/ubuntu-22.04-autoinstall.iso --add-debs-to-pool $_OUTPUT/puppet-omnibus_3.8.5-exo4_amd64.deb 
